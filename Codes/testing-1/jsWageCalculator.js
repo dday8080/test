4c172991217e16b83b4713bfe436789abb00fda5
dday8080 <101784232+dday8080@users.noreply.github.com>
2022-05-13 01:24:14 -0600

    Add files via upload

---
+++ Codes/testing-1/jsWageCalculator.js
@@ -0,0 +1,27 @@
+// Wage Calculator JS
+
+let element = document.getElementById('wageCalculator');
+
+let cc = console.log;
+
+element.innerHTML += 'jsWageCalculator';
+
+let form = document.getElementById("inputValues");
+
+form.onclick = function(e) {
+    e.preventDefault();
+        let inputPerHour = document.querySelector("#inputPerHour").value;
+        let inputHoursWeek = document.querySelector("#inputHoursWeek").value;
+        let overtime = document.querySelector("#overtime");
+    cc(inputPerHour);
+    cc(inputHoursWeek);
+    cc(overtime);
+    document.getElementById('print').innerHTML=inputPerHour;
+    document.getElementById('printer').innerHTML=inputHoursWeek;
+}
+
+//cc(document.getElementById("input-values", "inputPerHour", "inputHoursWeek").addEventListener("click", function () {
+   // form.submit();
+//}));
+
+cc(inputHoursWeek * inputPerHour);
